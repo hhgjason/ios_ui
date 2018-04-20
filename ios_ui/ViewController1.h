fcb2fec5c9b3c3ee350a60f6421efa5f4a809de1
hhjjason <hhuagui@163.com>
2018-04-20 21:24:36 +0800

    增加viewcontroller1
    增加viewcontroller1,里面实现了视频功能。

---
+++ ios_ui/ViewController1.h
@@ -0,0 +1,13 @@
+//
+//  ViewController1.h
+//  ios_ui
+//
+//  Created by huagui.hu on 2018/4/20.
+//  Copyright © 2018年 huagui.hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController1 : UITabBarController
+
+@end
